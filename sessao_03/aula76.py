79c19169e4d882888b2769df31bd1bafdbd9c0db
mariodac <mdac.mario@gmail.com>
2024-01-10 21:39:36 -0300

    upgrade quiz

--- sessao_03/aula76.py
+++ sessao_03/aula76.py
@@ -20,7 +20,12 @@ from platform import system as os_name
 palavra_secreta = 'python'
 letra_acertadas = ''
 tentativas = 0
+maximo_tentativas = len(palavra_secreta) * 4
+tentativas_restantes = maximo_tentativas
+dica_index = 0
+dicas = ['é uma linguagem de programação', 'foi criado por um programador holandês', 'foi influenciado pela linguagem C', 'possui o nome de animal', 'é uma linguagem interpretada', 'é uma linguagem multiparadigma', 'linguagem possui tipagem dinamica e forte']
 while True:
+    print(f'Você possui {tentativas_restantes} tentativas restantes')
     letra = input('Digite uma letra: ')
     if len(letra) > 1:
         print('Você deve digitar apenas uma letra!')
@@ -30,30 +35,53 @@ while True:
         continue
     else:
         tentativas += 1
+        tentativas_restantes -=  1
         if letra in palavra_secreta:
             letra_acertadas += letra
-            astericos = 0
+            palavra_formada = ''
             for letra_secreta in palavra_secreta:
                 if letra_secreta in letra_acertadas:
-                    print(letra_secreta, end='')
+                    palavra_formada += letra_secreta
                 else:
-                    astericos += 1
-                    print('*', end='')
-            print('')
-            if astericos == 0:
-                print('Parabéns, você ganhou!')
-                print(f'A palavra secreta é "{palavra_secreta}"')
-                print(f'Numero de tentivas: {tentativas}')
-                sair = input('Deseja sair? (S)im =>')
-                if (sair.lower() == 's') or (sair.lower() == 'sim'): break
+                    palavra_formada += '*'
+            print(palavra_formada)
+            if palavra_formada == palavra_secreta:
                 if os_name() == 'Windows':
                     system('cls')
                 else:
                     system('clear')
+                print('Parabéns, você ganhou!'.upper())
+                print(f'A palavra secreta é "{palavra_secreta}"')
+                print(f'Numero de tentivas: {tentativas}')
+                sair = input('Deseja sair? (S)im =>')
+                if (sair.lower() == 's') or (sair.lower() == 'sim'): break
                 tentativas = 0
                 letra_acertadas = ''
-                
+                maximo_tentativas = len(palavra_secreta) * 4
+                tentativas_restantes = maximo_tentativas
+                dica_index = 0
+        
+        elif tentativas == maximo_tentativas:
+            if os_name() == 'Windows':
+                system('cls')
+            else:
+                system('clear')
+            print('Você perdeu!')
+            print(f'A palavra secreta é "{palavra_secreta}"')
+            print(f'Numero de tentativas: {tentativas}')
+            sair = input('Deseja sair? (S)im =>')
+            if (sair.lower() =='s') or (sair.lower() =='sim'): break
+            tentativas = 0
+            letra_acertadas = ''
+            maximo_tentativas = len(palavra_secreta) * 4
+            tentativas_restantes = maximo_tentativas
+            dica_index = 0
         else:
+            if tentativas % 2 == 0:
+                if dica_index == len(dicas):
+                    print('Você esgotou todas as dicas'.upper())
+                else:
+                    print('Aqui vai uma dica:')
+                    print(f'{dicas[dica_index].upper()}')
+                    dica_index += 1
             continue
-
-
