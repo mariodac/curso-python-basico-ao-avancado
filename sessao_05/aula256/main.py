9c931fa874e41401121a562247da7bac4fd5aa78
mariodac <mdac.mario@gmail.com>
2024-05-25 05:19:31 -0300

    Desenvolvimento do exercicio

--- sessao_05/aula256/main.py
+++ sessao_05/aula256/main.py
@@ -39,12 +39,13 @@ while option != 0:
                     agency = choice(bank.agencies)
                     account_number = randint(1, 1000)
                     account = CheckingAccounts(agency,  account_number, limit=100)
-                    bank.clients[client_index]._account = account
+                    bank.clients[client_index-1]._account = account
                 elif type_account == 2:
                     agency = choice(bank.agencies)
                     account_number = randint(1, 1000)
                     account = SavingAccounts(agency, account_number)
-                    bank.clients[client_index]._account = account
+                    bank.clients[client_index-1]._account = account
+                print(f'Dados de conta criada: {agency=} - {account_number=}')
                 bank.accounts.append(account)
             else:
                 print('Opção inválida')
@@ -56,7 +57,22 @@ while option != 0:
             print(f'{index+1} - {client_item.name}')
         client_index = only_int(input('Digite o numero do cliente => '))
         if client_index-1 in [x for x in range(len(bank.clients))]:
-            value = only_int(input('Digite o valor a depositar =>'))
-            bank.clients[client_index]._account.cash_deposit(value)
+            if hasattr(bank.clients[client_index-1], '_account'):
+                value = only_int(input('Digite o valor a depositar =>'))
+                bank.clients[client_index-1]._account.cash_deposit(value)
+            else:
+                print('Depósito não realizado, cliente não possui conta')
+        else:
+            print('Cliente não encontrado')
     elif option == 4:
-        ...
+        for index, client_item in enumerate(bank.clients):
+            print(f'{index+1} - {client_item.name}')
+        client_index = only_int(input('Digite o numero do cliente => '))
+        if client_index-1 in [x for x in range(len(bank.clients))]:
+            if hasattr(bank.clients[client_index-1], '_account'):
+                value = only_int(input('Digite o valor a sacar =>'))
+                bank.clients[client_index-1]._account.withdraw_money(value)
+            else:
+                print('Saque não realizado, cliente não possui conta')
+        else:
+            print('Cliente não encontrado')
