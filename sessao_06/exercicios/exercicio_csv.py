17cc94a211f33450679544f90c6b8483c8a91d0e
mariodac <mdac.mario@gmail.com>
2024-06-28 06:56:55 -0300

    Mais exercicios resolvidos

--- sessao_06/exercicios/exercicio_csv.py
+++ sessao_06/exercicios/exercicio_csv.py
@@ -22,7 +22,7 @@
 4. Cálculo de Estatísticas de Vendas:
 
     Crie um script que leia os dados de vendas de um arquivo CSV chamado vendas.csv.
-    O arquivo contém as seguintes colunas: id_pedido, produto, quantidade, valor_unitario e valor_total.
+    O arquivo contém as seguintes colunas: id_pedido, produto, quantidade, valor_unitario, valor_total, data_pedido.
     Calcule e exiba na tela as seguintes estatísticas:
         Total de vendas por produto
         Ticket médio (valor total das vendas / número de vendas)
@@ -30,10 +30,10 @@
 
 5. Combinação de Dados de Múltiplos Arquivos CSV:
 
-    Desenvolva um programa que combine as informações de dois arquivos CSV: clientes.csv e pedidos.csv.
-    O arquivo usuarios.csv contém as colunas id_usuario, nome e email.
-    O arquivo pedidos.csv contém as colunas id_pedido, id_usuario, data_pedido, valor_total.
-    Gere um novo arquivo CSV chamado pedidos_completos.csv contendo as seguintes colunas:
+    Desenvolva um programa que combine as informações de dois arquivos CSV: clientes.csv e vendas.csv.
+    O arquivo clientes.csv contém as colunas id_usuario, nome, email, cidade e estado.
+    O arquivo vendas.csv contém as colunas id_pedido, id_usuario, data_pedido, valor_total, produto, quantidade, valor_unitario,.
+    Gere um novo arquivo CSV chamado vendas_completos.csv contendo as seguintes colunas:
         id_pedido
         nome_usuario (nome do usuário obtido a partir do id_usuario)
         data_pedido
@@ -42,23 +42,99 @@
 import csv
 from pathlib import Path
 
+def get_csv(filename:Path, mode:str):
+    file_csv = open(filename, mode, encoding='utf-8', newline='')
+    return file_csv
+
 clientes_path = Path(__file__).parent / 'clientes.csv'
 collumns_names = ['id_usuario', 'nome', 'email', 'cidade', 'estado']
-with open(clientes_path, 'r', encoding='utf-8') as file_csv:
-    reader = csv.DictReader(file_csv)
-    print(' | '.join(collumns_names))
-    for line in reader:
-        print(f'{line['id_usuario']} | {line['nome']} | {line['email']} | {line['cidade']} | {line['estado']}')
+# inicio exercicio 1
+file_csv = get_csv(clientes_path, 'r')
+reader = csv.DictReader(file_csv)
+print(' | '.join(collumns_names))
+for line in reader:
+    print(f'{line['id_usuario']} | {line['nome']} | {line['email']} | {line['cidade']} | {line['estado']}')
     last_id = int(line['id_usuario'])
-    print(last_id)
+file_csv.close()
+# fim exercicio 1
 
-new_id = str(last_id+1)
-name = input('Digite o nome => ')
-email = input('Digite o email => ')
-city = input('Digite a cidade => ')
-estate = input('Digite o estado => ')
-client = {'id_usuario' : new_id, 'nome' : name, 'email' : email, 'cidade' : city, 'estado' : estate}
-with open(clientes_path, 'a', encoding='utf-8',  newline='') as file_csv:
-    writer = csv.DictWriter(file_csv, fieldnames=collumns_names)
-    writer.writerow(client)
-        
+# inicio exercicio 2
+# new_id = str(last_id+1)
+# name = input('Digite o nome => ')
+# email = input('Digite o email => ')
+# city = input('Digite a cidade => ')
+# estate = input('Digite o estado => ')
+# client = {'id_usuario' : new_id, 'nome' : name, 'email' : email, 'cidade' : city, 'estado' : estate}
+# file_csv = get_csv(clientes_path, 'a')
+# writer = csv.DictWriter(file_csv, fieldnames=collumns_names)
+# writer.writerow(client)
+# file_csv.close()
+# fim exercicio 2
+
+# inicio exercicio 3
+# email = input('Digte o email => ')
+# file_csv = get_csv(clientes_path, 'r')
+# reader = csv.DictReader(file_csv)
+# list_reader = []
+# not_found = True
+# index = 0
+# for line in reader:
+#     list_reader.append(line)
+#     if email == line['email']:
+#         print(f'Cidade atual => {line['cidade']}')
+#         city = input('Digite a cidade => ')
+#         print(f'Estado atual => {line['estado']}')
+#         estate = input('Digite o estado => ')
+#         list_reader[index]['cidade'] = city
+#         list_reader[index]['estado'] = estate
+#         not_found = False
+#     index += 1
+# file_csv.close()
+# if not_found:
+#     print('Email não encontrado')
+# file_csv = get_csv(clientes_path, 'w')
+# writer = csv.DictWriter(file_csv, fieldnames=collumns_names)
+# writer.writeheader()
+# for line in list_reader: 
+#     writer.writerow(line)
+# file_csv.close()
+# fim exercicio 3
+
+# inicio exercicio 4
+# venda_file = Path(__file__).parent / 'vendas.csv'
+# file_csv = get_csv(venda_file, 'r')
+# reader = csv.DictReader(file_csv)
+# list_reader = [x for x in reader]
+# total_qtd = 0
+# total_value = 0
+# sales_by_product = {}
+# medium_ticket = 0
+# most_value_total = 0
+# most_value_product = ''
+# for item_index, item in enumerate(list_reader):
+#     total_qtd = int(item['quantidade'])
+#     total_value = float(item['valor_total'].replace(',', '.'))
+#     for new_item_index,new_item in enumerate(list_reader):
+#         if item_index != new_item_index:
+#             if item['produto'] == new_item['produto']:
+#                 total_qtd += int(new_item['quantidade'])
+#                 total_value += float(new_item['valor_total'].replace(',', '.'))
+#         if total_value > most_value_total:
+#             most_value_total = total_value
+#             most_value_product = item['produto']
+#         medium_ticket = total_value / total_qtd
+#         sales_by_product.update({item['produto']:[total_qtd, medium_ticket]})
+# for item in sales_by_product:
+#     print(f'Total vendas do produto {item}: {sales_by_product[item][0]}')
+#     print(f'Ticket médio do produto {item}: {sales_by_product[item][1]}')
+#     print()
+# print(f'O produto maior valor total vendido foi: {most_value_product}')
+# file_csv.close()
+# fim exercicio 4
+
+# inicio exercicio 5
+complete_sales = Path(__file__).parent / 'vendas_completos.csv'
+file_clientes_csv = get_csv(clientes_path, 'r')
+reader = csv.DictReader(file_clientes_csv)
+list_clientes = []
+collumns_names = ["id_pedido", "nome_usuario", "data_pedido", "valor_total"]
