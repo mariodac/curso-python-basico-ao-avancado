5d632568a3dd3732286afd39ff49f7b2d992b1a9
mariodac <mdac.mario@gmail.com>
2024-07-28 06:18:06 -0300

    aula 341

--- sessao_07/README.md
+++ sessao_07/README.md
@@ -3,3 +3,4 @@
 - 338 - **O que é PySide6 e o motivo da substituição de PyQT5 ->** *[aula338](aula338.md)*
 - 339 - **Seção antiga com PyQT5** *[aula339](./aulaPyQT/)*
 - 340 - **Instalando o PySide6 no seu ambiente virtual ->** *[aula340](aula340.py)*
+- 341 - **QApplication e QPushButton de PySide6.QtWidgets ->** *[aula341](aula341.py)*
---
+++ sessao_07/aula341.py
@@ -0,0 +1,18 @@
+# QApplication e QPushButton de pySide6.QtWidgets
+# QApplication -> O Widget principal da aplicação
+# QPushButton -> Um botão
+# PySide6.Widgets -> On de estão os widgets do PySide6
+import sys
+from PySide6.QtWidgets import QApplication, QPushButton
+
+app = QApplication(sys.argv)
+
+button = QPushButton('Botão 1') 
+button.setStyleSheet('font-size: 40px; color: red;')
+button.show() # Adicionar o widget na hierarquia e exibe a janela
+
+# button2 = QPushButton('Botão 2')
+# button2.show()
+
+
+app.exec() # O loop da aplicção
