030f91614a91e176825539a67a0c1dc562e5ab9a
mariodac <mdac.mario@gmail.com>
2024-10-03 21:10:39 -0300

    aula 389

--- sessao_08/aula_sqlite/main.py
+++ sessao_08/aula_sqlite/main.py
@@ -1,14 +1,27 @@
 import sqlite3
 from pathlib import Path
 
+# SQLite tutorial - https://www.techonthenet.com/sqlite/index.php
+# SQLite Documentação - https://www.sqlite.org/doclist.html
+# DBeaver CE - https://dbeaver.io/download/
+
 ROOT_DIR = Path(__file__).parent
-DB_NAME = 'db.sqlite3'
+DB_NAME = "db.sqlite3"
 DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = "customers"
 
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
-# SQL
+cursor.execute(
+    f"CREATE TABLE IF NOT EXISTS {TABLE_NAME}"
+    "("
+    "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+    "name TEXT,"
+    "weight REAL"
+    ")"
+)
+connection.commit()
 
 cursor.close()
 connection.close()
