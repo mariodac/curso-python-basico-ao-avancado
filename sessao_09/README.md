d79613cfd5c3791aacd6ceae2dc3f19635fd769c
mariodac <mdac.mario@gmail.com>
2025-01-16 05:50:21 -0300

    Aula 429

--- sessao_09/README.md
+++ sessao_09/README.md
@@ -11,3 +11,4 @@
 - 426 - **Movendo as function based views para o arquivo views.py dos novos apps no Django ->** *[aula426](./ola_django/)*
 - 427 - **Aninhando URLs com path, include e urls.py dos apps do Django ->** *[aula427](./ola_django/)*
 - 428 - **Renderizando HTML, render, templates, INSTALLED_APPS e TemplateDoesNotExist ->** *[aula428](./ola_django/)*
+- 429 - **Configurando templates globais com DIRS + extends para herança de templates ->** *[aula429](./ola_django/)*
--- sessao_09/ola_django/blog/views.py
+++ sessao_09/ola_django/blog/views.py
@@ -5,6 +5,6 @@ def index(request):
     return render(request,"blog/blog.html")
 
 
-# foi criado o namespace, onde é criado uma pasta com o mesmo nome do app (blog/template/blog/*.html), assim evita a colisão de nomes de arquivos iguais em outros apps
+# foi criado o namespace, onde é criado uma pasta com o mesmo nome do app (blog/templates/blog/*), assim evita a colisão de nomes de arquivos iguais em outros apps
 def exemplo(request):
     return render(request, "blog/exemplo.html")
