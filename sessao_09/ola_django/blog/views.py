a7b7081db9a2f5553318f006bc69e7dadffc6d87
mariodac <mdac.mario@gmail.com>
2025-01-28 06:51:18 -0300

    Randonizando posts e adicionado tag para quando não tiver nenhum post

--- sessao_09/ola_django/blog/views.py
+++ sessao_09/ola_django/blog/views.py
@@ -2,49 +2,35 @@ from typing import Any
 from django.http import HttpRequest, Http404
 from django.shortcuts import render
 from blog.data import get_posts
+from random import shuffle
+
 
 # Create your views here.
 def index(request):
     data = get_posts()
+    shuffle(data)
     content = {
-        'title' : 'Todos os posts',
-        'posts' : data
+        "title": "Todos os posts",
+        "posts": data,
     }
-    return render(
-        request,
-        "blog/blog.html",
-        content
-    )
+    return render(request, "blog/blog.html", content)
 
 
 # foi criado o namespace, onde é criado uma pasta com o mesmo nome do app (blog/templates/blog/*), assim evita a colisão de nomes de arquivos iguais em outros apps
 def exemplo(request):
-    content = {
-        'text': 'Bem-vindo ao exemplo!',
-        'title': 'Página exemplo do blog'
-    }
-    return render(
-        request, 
-        "blog/exemplo.html",
-        content
-    )
+    content = {"text": "Bem-vindo ao exemplo!", "title": "Página exemplo do blog"}
+    return render(request, "blog/exemplo.html", content)
+
 
 def post(request: HttpRequest, post_id):
     found_post: dict[str, Any] | None = None
     data = get_posts()
     for post_item in data:
-        if post_item['id'] == post_id:
+        if post_item["id"] == post_id:
             found_post = post_item
             break
     print(found_post)
     if found_post is None:
         raise Http404("Post não encontrado")
-    content = {
-        'title' : f'Post {found_post["title"]}',
-        'post' : found_post
-    }
-    return render(
-        request,
-        "blog/post.html",
-        content
-    )
+    content = {"title": f'Post {found_post["title"]}', "post": found_post}
+    return render(request, "blog/post.html", content)
--- sessao_09/ola_django/home/views.py
+++ sessao_09/ola_django/home/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from blog.data import get_posts
+from random import shuffle
 
 # Create your views here.
 
 def index(request):
+    data = get_posts()
+    shuffle(data)
     context = {
             'text': 'Olá Mundo!',
-            'title': 'Bem-vindo'
+            'title': 'Bem-vindo',
+            'posts': data[:5],
         }
     return render(
         request, 
