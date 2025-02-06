90d02d0a6af4888d76dbcf5bd8f13de612003bc5
mariodac <mdac.mario@gmail.com>
2025-02-06 20:40:35 -0300

    Tradução Django
    - Tradução do template base
    - Tradução das models

--- sessao_10/agenda/contact/models.py
+++ sessao_10/agenda/contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
 # id (primary key - automático)
@@ -10,23 +11,26 @@ from django.utils import timezone
 
 class Category(models.Model):
     class Meta:
-        verbose_name = 'Category'
-        verbose_name_plural = 'Categories'
-    name = models.CharField(max_length=50)
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+    name = models.CharField(max_length=50, verbose_name=_("Name"))
 
     def __str__(self) -> str:
         return self.name
     
 class Contact(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50, blank=True)
-    phone = models.CharField(max_length=20)
+    class Meta:
+        verbose_name = _('Contact')
+        verbose_name_plural = _('Contacts')
+    first_name = models.CharField(max_length=50, verbose_name=_("First Name"))
+    last_name = models.CharField(max_length=50, blank=True, verbose_name=_("Last Name"))
+    phone = models.CharField(max_length=20, verbose_name=_("Phone"))
     email = models.EmailField(max_length=254, blank=True)
-    created_date = models.DateTimeField(default=timezone.now)
-    description = models.TextField(blank=True)
-    show = models.BooleanField(default=True)
-    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/%d')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now, verbose_name=_("Created Date"))
+    description = models.TextField(blank=True, verbose_name=_("Description"))
+    show = models.BooleanField(default=True, verbose_name=_("Show"))
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/%d', verbose_name=_("Picture"))
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, verbose_name=_("Category"))
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
