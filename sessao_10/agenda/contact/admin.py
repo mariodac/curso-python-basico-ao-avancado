279664cd41425cc3302fcaf638c4acf685a4c39a
mariodac <mdac.mario@gmail.com>
2025-03-03 01:08:02 -0300

    Aula 467
    Manipulando QuerySets Django com filter, order_by e fatiamento
    Adicionado mais traduções

--- sessao_10/agenda/contact/admin.py
+++ sessao_10/agenda/contact/admin.py
@@ -1,19 +1,43 @@
 from django.contrib import admin
 from contact import models
 
+
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'phone', 'email', 'created_date',)
-    ordering = ('-first_name',)
-    list_filter = ('created_date','category')
-    search_fields = ('first_name', 'last_name', 'phone',)
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "phone",
+        "email",
+        "created_date",
+        "show",
+    )
+    ordering = ("-first_name",)
+    list_filter = ("created_date", "category")
+    search_fields = (
+        "first_name",
+        "last_name",
+        "phone",
+    )
     list_per_page = 10
     list_max_show_all = 100
-    list_editable = ('first_name', 'last_name', 'email',)
-    list_display_links = ('id', 'phone')
+    list_editable = (
+        "first_name",
+        "last_name",
+        "email",
+        "show",
+    )
+    list_display_links = ("id", "phone")
 
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name',)
-    ordering = ('id', 'name',)
+    list_display = (
+        "id",
+        "name",
+    )
+    ordering = (
+        "id",
+        "name",
+    )
--- sessao_10/agenda/contact/views/contact_views.py
+++ sessao_10/agenda/contact/views/contact_views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-
+from django.utils import translation
+from django.utils.translation import gettext as _
 from contact.models import Contact
 
 def index(request):
-    contacts = Contact.objects.all()
-
+    contacts = Contact.objects.all().filter(show=True).order_by('id')[:10]
+    print(contacts.query)
     context = {
         'contacts': contacts,
+        'title': _('Contacts'),
+        'html_language': translation.get_language(),
     }
     return render(
         request,
