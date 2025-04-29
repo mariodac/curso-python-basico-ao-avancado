4e060562d85ab4bf60f5e43db0d9b3e9052f18e4
mariodac <mdac.mario@gmail.com>
2025-04-28 22:35:28 -0300

    Aula 518
    Configurando variáveis de ambiente com .env e o settings.py do projeto Django

--- sessao_12/README.md
+++ sessao_12/README.md
@@ -5,4 +5,5 @@
 - 514 - **Iniciando o projeto e configurando git, GitHub e .gitignore**
 - 515 - **Configurando o VS Code (.vscode/settings.json)**
 - 516 - **Iniciando o projeto Django com django-admin startproject**
-- 517 - **Criando arquivos requirements.txt e .dockerignore**
+- 517 - **Criando arquivos requirements.txt e .dockerignore ->** *[aula517](./sessao_12/)*
+- 518 - **Configurando variáveis de ambiente com .env e o settings.py do projeto Django ->** *[aula518](./sessao_12/)*
--- sessao_12/djangoapp/project/settings.py
+++ sessao_12/djangoapp/project/settings.py
@@ -10,22 +10,29 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# /data/web/static
+# /data/web/media
+DATA_DIR = BASE_DIR / 'data' / 'web'
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$qxfa(nr7s5jtw5%b@#=-*k#091grm0h)@=^_=!$6o*h(pmki-'
+SECRET_KEY = os.getenv('SECRET_KEY', 'change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',')
+    if h.strip()
+]
 
 
 # Application definition
@@ -74,8 +81,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('POSTGRES_DB', 'change-me'),
+        'USER': os.getenv('POSTGRES_USER', 'change-me'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'change-me'),
+        'HOST': os.getenv('POSTGRES_HOST', 'change-me'),
+        'PORT': os.getenv('POSTGRES_PORT', 'change-me'),
     }
 }
 
@@ -102,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -115,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = DATA_DIR / 'static'
+
+MEDIA_URL = 'static/'
+MEDIA_ROOT = DATA_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
